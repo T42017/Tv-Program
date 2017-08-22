4faa9b1e3fafb9203f6eae9b1d34508296a1ed83
Mathias Andersson <mathias.andersson@lund.ntig.se>
2017-08-22 21:28:29 +0200

    Latest version. Missing one or two things, some bugs need fixing.

--- Tv Browser/Tv Browser/Form1.Designer.cs
+++ Tv Browser/Tv Browser/Form1.Designer.cs
@@ -29,36 +29,32 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.titlebox = new System.Windows.Forms.TextBox();
             this.dayChooser = new System.Windows.Forms.ListBox();
             this.previousTime = new System.Windows.Forms.Button();
             this.nextTime = new System.Windows.Forms.Button();
             this.airingTime = new System.Windows.Forms.TextBox();
-            this.subTitleBox = new System.Windows.Forms.TextBox();
             this.descriptionTextBox = new System.Windows.Forms.TextBox();
-            this.episodeNumberBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.episodeNumberLabel = new System.Windows.Forms.Label();
+            this.currentlyOnAirLink = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nextOnAirLink = new System.Windows.Forms.LinkLabel();
+            this.timer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Items.AddRange(new object[] {
-            "travelchanneltv.se"});
+            "travelchanneltv.se",
+            "comedycentral.tv"});
             this.listBox1.Location = new System.Drawing.Point(12, 11);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(109, 615);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.tvChannelList_SelectedIndexChanged);
-            // 
-            // titlebox
-            // 
-            this.titlebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titlebox.Location = new System.Drawing.Point(253, 11);
-            this.titlebox.Name = "titlebox";
-            this.titlebox.Size = new System.Drawing.Size(592, 38);
-            this.titlebox.TabIndex = 1;
-            this.titlebox.TextChanged += new System.EventHandler(this.titlebox_TextChanged);
             // 
             // dayChooser
             // 
@@ -91,65 +87,130 @@
             // 
             // airingTime
             // 
+            this.airingTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.airingTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.airingTime.Location = new System.Drawing.Point(932, 12);
+            this.airingTime.Location = new System.Drawing.Point(932, 17);
             this.airingTime.Name = "airingTime";
-            this.airingTime.Size = new System.Drawing.Size(324, 38);
+            this.airingTime.Size = new System.Drawing.Size(324, 31);
             this.airingTime.TabIndex = 5;
+            this.airingTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.airingTime.TextChanged += new System.EventHandler(this.airingTime_TextChanged);
-            // 
-            // subTitleBox
-            // 
-            this.subTitleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.subTitleBox.Location = new System.Drawing.Point(253, 56);
-            this.subTitleBox.Name = "subTitleBox";
-            this.subTitleBox.Size = new System.Drawing.Size(592, 30);
-            this.subTitleBox.TabIndex = 6;
-            this.subTitleBox.TextChanged += new System.EventHandler(this.subTitleBox_TextChanged);
             // 
             // descriptionTextBox
             // 
+            this.descriptionTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.descriptionTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.descriptionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.descriptionTextBox.Location = new System.Drawing.Point(253, 130);
+            this.descriptionTextBox.Location = new System.Drawing.Point(253, 163);
             this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
-            this.descriptionTextBox.Size = new System.Drawing.Size(592, 393);
+            this.descriptionTextBox.Size = new System.Drawing.Size(592, 463);
             this.descriptionTextBox.TabIndex = 7;
             this.descriptionTextBox.TextChanged += new System.EventHandler(this.descriptionTextBox_TextChanged);
             // 
-            // episodeNumberBox
+            // titleLabel
             // 
-            this.episodeNumberBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.episodeNumberBox.Location = new System.Drawing.Point(582, 94);
-            this.episodeNumberBox.Name = "episodeNumberBox";
-            this.episodeNumberBox.Size = new System.Drawing.Size(263, 30);
-            this.episodeNumberBox.TabIndex = 8;
-            this.episodeNumberBox.TextChanged += new System.EventHandler(this.episodeNumberBox_TextChanged);
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(253, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(109, 39);
+            this.titleLabel.TabIndex = 9;
+            this.titleLabel.Text = "label1";
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.AutoSize = true;
+            this.subTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.Location = new System.Drawing.Point(253, 58);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(115, 25);
+            this.subTitleLabel.TabIndex = 10;
+            this.subTitleLabel.Text = "episode title";
+            this.subTitleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // episodeNumberLabel
+            // 
+            this.episodeNumberLabel.AutoSize = true;
+            this.episodeNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.episodeNumberLabel.Location = new System.Drawing.Point(661, 102);
+            this.episodeNumberLabel.Name = "episodeNumberLabel";
+            this.episodeNumberLabel.Size = new System.Drawing.Size(184, 25);
+            this.episodeNumberLabel.TabIndex = 11;
+            this.episodeNumberLabel.Text = "Episode 6 season 3";
+            // 
+            // currentlyOnAirLink
+            // 
+            this.currentlyOnAirLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentlyOnAirLink.Location = new System.Drawing.Point(956, 293);
+            this.currentlyOnAirLink.Name = "currentlyOnAirLink";
+            this.currentlyOnAirLink.Size = new System.Drawing.Size(300, 29);
+            this.currentlyOnAirLink.TabIndex = 12;
+            this.currentlyOnAirLink.TabStop = true;
+            this.currentlyOnAirLink.Text = "Loading...";
+            this.currentlyOnAirLink.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.currentlyOnAirLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.currentlyOnAirLink_LinkClicked);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(253, 15);
+            this.label1.Location = new System.Drawing.Point(998, 236);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 31);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Currently On Air:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(967, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 31);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Next On Air In:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nextOnAirLink
+            // 
+            this.nextOnAirLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextOnAirLink.Location = new System.Drawing.Point(956, 454);
+            this.nextOnAirLink.Name = "nextOnAirLink";
+            this.nextOnAirLink.Size = new System.Drawing.Size(300, 33);
+            this.nextOnAirLink.TabIndex = 15;
+            this.nextOnAirLink.TabStop = true;
+            this.nextOnAirLink.Text = "Loading...";
+            this.nextOnAirLink.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.Location = new System.Drawing.Point(1164, 390);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(82, 31);
+            this.timer.TabIndex = 16;
+            this.timer.Text = "00:00";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1315, 638);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.nextOnAirLink);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.episodeNumberBox);
+            this.Controls.Add(this.currentlyOnAirLink);
+            this.Controls.Add(this.episodeNumberLabel);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.descriptionTextBox);
-            this.Controls.Add(this.subTitleBox);
             this.Controls.Add(this.airingTime);
             this.Controls.Add(this.nextTime);
             this.Controls.Add(this.previousTime);
             this.Controls.Add(this.dayChooser);
-            this.Controls.Add(this.titlebox);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -161,15 +222,19 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox titlebox;
         private System.Windows.Forms.ListBox dayChooser;
         private System.Windows.Forms.Button previousTime;
         private System.Windows.Forms.Button nextTime;
         private System.Windows.Forms.TextBox airingTime;
-        private System.Windows.Forms.TextBox subTitleBox;
         private System.Windows.Forms.TextBox descriptionTextBox;
-        private System.Windows.Forms.TextBox episodeNumberBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label episodeNumberLabel;
+        private System.Windows.Forms.LinkLabel currentlyOnAirLink;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel nextOnAirLink;
+        private System.Windows.Forms.Label timer;
     }
 }
 
--- Tv Browser/Tv Browser/Form1.cs
+++ Tv Browser/Tv Browser/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -37,6 +38,42 @@ namespace Tv_Browser
             listBox1.SelectedIndex = 0;
             dayChooser.SelectedIndex = 10;
 
+            StartTimer();
+        }
+
+        System.Windows.Forms.Timer time = null;
+        private void StartTimer()
+        {
+            time = new System.Windows.Forms.Timer();
+            time.Interval = 1000;
+            time.Tick += new EventHandler(t_Tick);
+            time.Enabled = true;
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            DateTime nextShow = GetNextShowTime();
+            var time = nextShow - DateTime.Now;
+            string formattedTimespan = time.ToString("hh\\:mm\\:ss");
+            timer.Text = formattedTimespan;
+            
+            
+        }
+
+        private DateTime GetNextShowTime()
+        {
+            DateTime nextShowAiringTime = new DateTime(2000,10,1);
+
+            if (RemoteFileExists("http://xmltv.xmltv.se/" + listBox1.SelectedItem + "_" + dayChooser.SelectedItem + ".xml.gz"))
+            {
+                XDocument tvList = XDocument.Load("http://xmltv.xmltv.se/" + listBox1.SelectedItem + "_" + dayChooser.SelectedItem + ".xml.gz");
+                var tempData = tvList.Descendants("tv");
+                var airingTimeDataStart = tempData.Elements("programme").ElementAt(timeIndex).Attribute("start").Value;
+                DateTime start = DateTimeOffset.ParseExact(airingTimeDataStart, "yyyyMMddHHmmss zzz", System.Globalization.CultureInfo.InvariantCulture).DateTime;
+
+                return start;
+            }
+            return nextShowAiringTime;
         }
 
         private void tvChannelList_SelectedIndexChanged(object sender, EventArgs e)
@@ -63,33 +100,89 @@ namespace Tv_Browser
 
         }
 
+        private bool RemoteFileExists(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TRUE if the Status code == 200
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                //Any exception will returns false.
+                return false;
+            }
+        }
+
         public void LoadContent()
         {
 
-
+            
             var url = "http://xmltv.xmltv.se/" + listBox1.SelectedItem + "_" + dayChooser.SelectedItem + ".xml.gz";
-            XDocument tvList = XDocument.Load(url);
-            var tempData = tvList.Descendants("tv");
-            var title = tempData.Elements("programme").ElementAt(timeIndex).Element("title").Value;
-            var description = tempData.Elements("programme").ElementAt(timeIndex).Element("desc").Value;
-            var subTitle = tempData.Elements("programme").ElementAt(timeIndex).Element("sub-title").Value;
-            var episodeNumber = tempData.Elements("programme").ElementAt(timeIndex).Elements("episode-num").ElementAt(1).Value;
-            var airingTimeDataStart = tempData.Elements("programme").ElementAt(timeIndex).Attribute("start").Value;
-            DateTime start = DateTimeOffset.ParseExact(airingTimeDataStart, "yyyyMMddHHmmss zzz", System.Globalization.CultureInfo.InvariantCulture).DateTime;
-            var airingTimeDataStop = tempData.Elements("programme").ElementAt(timeIndex).Attribute("stop").Value;
-            DateTime stop = DateTimeOffset.ParseExact(airingTimeDataStop, "yyyyMMddHHmmss zzz", System.Globalization.CultureInfo.InvariantCulture).DateTime;
+
+            if (RemoteFileExists(url))
+            {
+                XDocument tvList = XDocument.Load(url);
+                var tempData = tvList.Descendants("tv");
+                var title = tempData.Elements("programme").ElementAt(timeIndex).Element("title").Value;
+                if (timeIndex != 0)
+                {
+                    if (timeIndex < amountOfShows - 1)
+                    {
+                        var title2 = tempData.Elements("programme").ElementAt(timeIndex + 1).Element("title").Value;
+                        nextOnAirLink.Text = title2;
+                    }  
+                    else
+                    {
+                        var title2 = tempData.Elements("programme").ElementAt(0).Element("title").Value;
+                        nextOnAirLink.Text = title2;
+                    }
+                    
+                }
+                var description = tempData.Elements("programme").ElementAt(timeIndex).Element("desc").Value;
+                if (tempData.Elements("programme").ElementAt(timeIndex).Element("sub-title") != null)
+                {
+                    var subTitle = tempData.Elements("programme").ElementAt(timeIndex).Element("sub-title").Value;
+                    subTitleLabel.Text = subTitle;
+                }
+                else
+                {
+                    subTitleLabel.Text = " ";
+                }
+                var episodeNumber = tempData.Elements("programme").ElementAt(timeIndex).Elements("episode-num")
+                    .ElementAt(1).Value;
+                var airingTimeDataStart = tempData.Elements("programme").ElementAt(timeIndex).Attribute("start").Value;
+                DateTime start = DateTimeOffset.ParseExact(airingTimeDataStart, "yyyyMMddHHmmss zzz", System.Globalization.CultureInfo.InvariantCulture).DateTime;
+                var airingTimeDataStop = tempData.Elements("programme").ElementAt(timeIndex).Attribute("stop").Value;
+                DateTime stop = DateTimeOffset.ParseExact(airingTimeDataStop, "yyyyMMddHHmmss zzz", System.Globalization.CultureInfo.InvariantCulture).DateTime;
+
+
+
+
+                descriptionTextBox.Text = description;
+                episodeNumberLabel.Text = episodeNumber;
+                amountOfShows = tempData.Elements("programme").Count();
+                titleLabel.Text = title;
+                airingTime.Text = start.TimeOfDay + " - " + stop.TimeOfDay;
+                currentlyOnAirLink.Text = title;
+            }
+            else
+            {
+                string Error = "No Info Exists, Sorry!";
+                descriptionTextBox.Text = " ";
+                episodeNumberLabel.Text = "Episode 0 season 0";
+                titleLabel.Text = Error;
+                airingTime.Text = " ";
+                subTitleLabel.Text = " ";
+            }
             
-            
-
-            subTitleBox.Text = subTitle;
-            descriptionTextBox.Text = description;
-            episodeNumberBox.Text = episodeNumber;
-            amountOfShows = tempData.Elements("programme").Count();
-            titlebox.Text = title;
-            airingTime.Text = start.TimeOfDay + " - " + stop.TimeOfDay;
-
-
-
         }
 
         private void airingTime_TextChanged(object sender, EventArgs e)
@@ -124,17 +217,22 @@ namespace Tv_Browser
             LoadContent();
         }
 
-        private void episodeNumberBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void subTitleBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void descriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void currentlyOnAirLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
         }
