665c4d4dd0d32697e48c13ab580ffc5d1f21d8dd
Marcus Linné <marcus.linne@lund.ntig.se>
2017-08-23 10:29:20 +0200

    first stage

---
+++ tvprog/tvprog/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace tvprog
+{
+    public partial class Form1 : Form
+    {
+        public DateTime SelDateTime=DateTime.Now;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           // string contents = "";
+          //  using (var wc = new System.Net.WebClient())
+            //    contents = wc.DownloadString("http://xmltv.xmltv.se/svt1.svt.se_2017-08-22.xml.gz");
+
+           // var t = StringXmlSerializer.Deserialize<tv>(contents);
+
+            XDocument url = XDocument.Load("http://xmltv.xmltv.se/svt1.svt.se_2017-08-22.xml.gz");
+            Debug.WriteLine(url);
+
+
+            Debug.WriteLine("laddar svt1822.xml ....");
+         //   var tmp = ObjectXMLSerializer<tv>.Load("svt1822.xml");
+            var tmp = url;
+            foreach (var program in url)
+            {
+                textBox2.AppendText("mm \r\n");
+                Debug.WriteLine("nytt program:");
+                Debug.WriteLine(program.title);
+                Debug.WriteLine(program.start);
+                Debug.WriteLine(program.stop);
+                Debug.WriteLine(program.GetStartDateTime());
+            }
+            textBox1.Text = SelDateTime.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelDateTime=SelDateTime.AddDays(-1);
+            textBox1.Text = SelDateTime.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SelDateTime = SelDateTime.AddDays(+1);
+            textBox1.Text = SelDateTime.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SelDateTime = DateTime.Now;
+            textBox1.Text = SelDateTime.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class tv
+    {
+        [XmlElement("programme")]
+        public List<programme> programs = new List<programme>();
+    }
+    public class programme
+    {
+        [XmlAttribute]
+        public string start;
+        [XmlAttribute]
+        public string stop;
+        public string title;
+
+        public DateTime GetStartDateTime()
+        {
+            return DateTimeOffset.ParseExact(start, "yyyyMMddHHmmss zzz", CultureInfo.InvariantCulture).DateTime;
+        }
+    }
+}
---
+++ tvprog/tvprog/ObjectXMLSerializer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace tvprog
+{
+
+    public class StringXmlSerializer
+    {
+        public static string Serialize<T>(T value)
+        {
+
+            if (value == null)
+            {
+                return null;
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = new UnicodeEncoding(false, false); // no BOM in a .NET string
+            settings.Indent = false;
+            settings.OmitXmlDeclaration = false;
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    serializer.Serialize(xmlWriter, value);
+                }
+                return textWriter.ToString();
+            }
+        }
+
+        public static T Deserialize<T>(string xml)
+        {
+
+            if (string.IsNullOrEmpty(xml))
+            {
+                return default(T);
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            XmlReaderSettings settings = new XmlReaderSettings() { DtdProcessing = DtdProcessing.Ignore };
+            // No settings need modifying here
+
+            using (StringReader textReader = new StringReader(xml))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(textReader, settings))
+                {
+                    return (T)serializer.Deserialize(xmlReader);
+                }
+            }
+        }
+    }
+
+    public static class ObjectXMLSerializer<T> where T : class
+    {
+        #region Publika medlemmar
+        public static T Load(string path)
+        {
+            T serializableObject = LoadFromDocumentFormat(null, path, null);
+            return serializableObject;
+        }
+
+        public static void Save(T serializableObject, string path)
+        {
+            SaveToDocumentFormat(serializableObject, null, path, null);
+        }
+        #endregion
+
+        #region Privata medlemmar
+        private static T LoadFromDocumentFormat(System.Type[] extraTypes, string path,
+            IsolatedStorageFile isolatedStorageFolder)
+        {
+            T serializableObject = null;
+
+            using (TextReader textReader = CreateTextReader(isolatedStorageFolder, path))
+            {
+                XmlSerializer xmlSerializer = CreateXmlSerializer(extraTypes);
+                serializableObject = xmlSerializer.Deserialize(textReader) as T;
+            }
+            return serializableObject;
+        }
+
+        private static TextReader CreateTextReader(IsolatedStorageFile isolatedStorageFolder, string path)
+        {
+            TextReader textReader = null;
+
+            if (isolatedStorageFolder == null)
+                textReader = new StreamReader(path);
+            else
+                textReader = new StreamReader(new IsolatedStorageFileStream(path,
+                    FileMode.Open, isolatedStorageFolder));
+
+            return textReader;
+        }
+
+        private static void SaveToDocumentFormat(T serializableObject,
+            System.Type[] extraTypes, string path, IsolatedStorageFile isolatedStorageFolder)
+        {
+            using (TextWriter textWriter = CreateTextWriter(isolatedStorageFolder, path))
+            {
+                XmlSerializer xmlSerializer = CreateXmlSerializer(extraTypes);
+                xmlSerializer.Serialize(textWriter, serializableObject);
+            }
+        }
+
+        private static TextWriter CreateTextWriter(IsolatedStorageFile
+                            isolatedStorageFolder, string path)
+        {
+            TextWriter textWriter = null;
+
+            if (isolatedStorageFolder == null)
+                textWriter = new StreamWriter(path);
+            else
+                textWriter = new StreamWriter(new IsolatedStorageFileStream(path,
+                                  FileMode.OpenOrCreate, isolatedStorageFolder));
+
+            return textWriter;
+        }
+
+        private static XmlSerializer CreateXmlSerializer(System.Type[] extraTypes)
+        {
+            Type ObjectType = typeof(T);
+
+            XmlSerializer xmlSerializer = null;
+
+            if (extraTypes != null)
+                xmlSerializer = new XmlSerializer(ObjectType, extraTypes);
+            else
+                xmlSerializer = new XmlSerializer(ObjectType);
+
+            return xmlSerializer;
+        }
+        #endregion
+    }
+}
