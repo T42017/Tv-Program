974be1e88bdca11625fa873e1b562a29cf08b407
Niklas Lindblom-Hertz <niklas.lindblomhertz@lund.ntig.se>
2017-08-23 11:06:05 +0200

    Channels fixed for downloaded GZ

---
+++ TV-program/TV-program/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TV_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.selectBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.Location = new System.Drawing.Point(12, 322);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(75, 23);
+            this.prevBtn.TabIndex = 0;
+            this.prevBtn.Text = "<----";
+            this.prevBtn.UseVisualStyleBackColor = true;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
+            // 
+            // selectBtn
+            // 
+            this.selectBtn.Location = new System.Drawing.Point(93, 322);
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectBtn.TabIndex = 0;
+            this.selectBtn.Text = "Select";
+            this.selectBtn.UseVisualStyleBackColor = true;
+            this.selectBtn.Click += new System.EventHandler(this.selectBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(174, 322);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextBtn.TabIndex = 0;
+            this.nextBtn.Text = "---->";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 304);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1461, 761);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.selectBtn);
+            this.Controls.Add(this.prevBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button selectBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TV-program/TV-program/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace TV_program
+{
+    public partial class Form1 : Form
+    {
+        List<string> tvAvsnitt = new List<string>();
+        List<string> tid = new List<string>();
+        int counter = 0;
+        public Form1()
+        {
+          InitializeComponent();
+            tvAvsnitt.Clear();
+            var reader = new XmlTextReader("viaFilm.gz");
+            while (reader.Read())
+            {
+
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "title")
+                    {
+                        tvAvsnitt.Add(reader.ReadInnerXml());
+                    }
+                    else if (reader.Name == "programme")
+                    {
+                        tid.Add(reader.GetAttribute("start"));
+                        tid.Add(reader.GetAttribute("stop"));
+                        textBox1.Text += tid[0];
+                    }
+                    
+                }
+
+            }
+            //textBox1.Text = tvAvsnitt[counter];
+
+
+        }
+        private void selectBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            counter++;
+
+            if (counter >= tvAvsnitt.Count)
+            {
+                counter = 0;
+                textBox1.Text = tvAvsnitt[counter];
+            }
+            else
+            {
+                textBox1.Text = tvAvsnitt[counter];
+            }
+        }
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter < 0)
+            {
+                counter = tvAvsnitt.Count -1;
+                textBox1.Text = tvAvsnitt[counter];
+            }
+            else
+            {
+            textBox1.Text = tvAvsnitt[counter];
+            }
+        }
+    }
+}
